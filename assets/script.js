bc23cc9d955bab0585bab7dfff0ab1be45006421
carterwsheppard <carterwsheppard@gmail.com>
2022-05-15 09:51:02 -0400

    initial HTML and first requirement: displaying current date at top of tile

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+//Get today's date and time and display that on the index file at the top
+var currentDay = moment().format('dddd, MMMM Do YYYY');
+$("#currentDay").html(currentDay);
+console.log(currentDate);
